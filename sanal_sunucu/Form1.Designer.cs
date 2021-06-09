5dfc2b81c49a5e47daa5554e937cfccb84f923a9
Ertuğrul Evrensel <73859360+ertugrulevrensel@users.noreply.github.com>
2021-06-09 16:04:09 +0300

    Virtual server project (Sanal sunucu projesi)

---
+++ sanal_sunucu/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace sanal_sunucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 177);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(797, 353);
+            this.flowLayoutPanel2.TabIndex = 0;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1311, 46);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(225, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SUNUCU İSTEK KONTROLÜ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(343, 53);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(120, 120);
+            this.panel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(577, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Alt Sunucu Sayısı   : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(577, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 27);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ana Sunucu Sayısı : 1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(750, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(821, 542);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private ns1.BunifuCircleProgressbar bunifuCircleProgressbar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ sanal_sunucu/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using ns1;
+
+namespace sanal_sunucu
+{
+    public partial class Form1 : Form
+    {
+        List<Thread> Th = new List<Thread>();
+        List<ns1.BunifuCircleProgressbar> pb = new List<ns1.BunifuCircleProgressbar>();
+        Button btn = new Button();
+        static object Kontrol = new object();
+
+        int MainVeri = 0;
+        int cek = 0;
+        List<int> ChildVeri = new List<int>();
+        int syc = -1;
+        int thcont = 0;
+        int qwedsa = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Thread thrA = new Thread(MainThread);
+            thrA.Start();
+        }
+
+        private void MainThread()
+        {
+            ns1.BunifuCircleProgressbar MainProgress = new ns1.BunifuCircleProgressbar();
+            if (this.InvokeRequired) //Forma gelen talebin farklı bir iş parçacığından gelip gelmediği kontrol ediliyor.
+            {
+                //Eğer farklı bir iş parçacığından talep gelmişse aşağıdaki Invoke metoduyla işlem gerçekleştiriliyor.
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    panel2.Controls.Add(MainProgress);
+                });
+            }
+
+            MainProgress.MaxValue = 10000;
+            MainProgress.Height = 120;
+            MainProgress.Width = 120;
+
+            Thread thr1 = new Thread(IstekKabulMain);
+            Thread thr2 = new Thread(IstekCevapMain);
+            thr1.Start();
+            thr2.Start();
+            MainProgress.BackColor = Color.Black;
+
+
+
+            MainProgress.Refresh();
+            Thread thrB = new Thread(childthread);
+            Thread thrC = new Thread(childthread);
+
+            label4.Text = qwedsa.ToString();
+
+            ChildVeri.Add(0);
+            ChildVeri.Add(0);
+            thrB.Start();
+            Thread.Sleep(100);
+            thrC.Start();
+
+
+
+            while (true)
+            {
+                MainProgress.Value = MainVeri;
+                MainProgress.Refresh();
+            }
+        }
+
+        private void childthread()
+        {
+            syc++;
+            Thread.CurrentThread.Name = syc.ToString();
+
+            ns1.BunifuCircleProgressbar ChildProgress = new ns1.BunifuCircleProgressbar();
+            if (this.InvokeRequired) //Forma gelen talebin farklı bir iş parçacığından gelip gelmediği kontrol ediliyor.
+            {
+                //Eğer farklı bir iş parçacığından talep gelmişse aşağıdaki Invoke metoduyla işlem gerçekleştiriliyor.
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    flowLayoutPanel2.Controls.Add(ChildProgress);
+                });
+            }
+            ChildProgress.MaxValue = 5000;
+            ChildProgress.Height = 130;
+            ChildProgress.Width = 130;
+            Thread thr3 = new Thread(IstekKabulChild);
+            Thread thr4 = new Thread(IstekCevapChild);
+            thr3.Name = Thread.CurrentThread.Name;
+            thr3.Start();
+            thr4.Name = Thread.CurrentThread.Name;
+            thr4.Start();
+            while (true)
+            {
+                Thread.Sleep(100);
+
+                ChildProgress.Value = ChildVeri[Int32.Parse(Thread.CurrentThread.Name)];
+                ChildProgress.Refresh();
+                if (((ChildProgress.Value * 100) / 5000) > 70)
+                {
+                    int knt = ChildProgress.Value / 2;
+                    Th.Add(new Thread(childthread));
+                    ChildVeri.Add(knt);
+                    ChildVeri[Int32.Parse(Thread.CurrentThread.Name)] -= knt;
+                    Th[thcont].Start();
+                    thcont++;
+                    qwedsa++;
+
+                    label4.Text = qwedsa.ToString();
+                    label4.Refresh();
+                }
+                else if (ChildProgress.Value == 0)
+                {
+                    if (qwedsa > 2)
+                    {
+                        flowLayoutPanel2.Controls.Remove(ChildProgress);
+                        thr3.Abort();
+                        thr4.Abort();
+                        qwedsa--;
+                        label4.Text = qwedsa.ToString();
+                        label4.Refresh();
+
+                        Thread.Sleep(Timeout.Infinite);
+
+
+                    }
+
+                }
+            }
+
+
+        }
+
+        private void IstekKabulMain()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                MainVeri += rnd.Next(1, 100);
+                Thread.Sleep(20);
+            }
+        }
+        private void IstekCevapMain()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                MainVeri -= rnd.Next(1, 50);
+                if (MainVeri < 0)
+                    MainVeri = 0;
+                Thread.Sleep(50);
+            }
+        }
+        public void IstekKabulChild()
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+                cek = rnd.Next(1, 50);
+                MainVeri -= cek;
+                if (MainVeri < 0)
+                {
+                    MainVeri += cek;
+                    ChildVeri[Int32.Parse(Thread.CurrentThread.Name)] += MainVeri;
+                    MainVeri = 0;
+                }
+                else
+                    ChildVeri[Int32.Parse(Thread.CurrentThread.Name)] += cek;
+                Thread.Sleep(30);
+            }
+        }
+        private void IstekCevapChild()
+        {
+
+            Random rnd = new Random();
+            while (true)
+            {
+                ChildVeri[Int32.Parse(Thread.CurrentThread.Name)] -= rnd.Next(1, 50);
+                if (ChildVeri[Int32.Parse(Thread.CurrentThread.Name)] < 0)
+                    ChildVeri[Int32.Parse(Thread.CurrentThread.Name)] = 0;
+                Thread.Sleep(50);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
